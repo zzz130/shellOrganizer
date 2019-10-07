e55d066cfadf02701a62c7a9d0ed846748e339c0
Ivan <z130@mail.ru>
2019-10-07 18:58:56 +0300

    initial commit

---
+++ src/main/java/com/karmanov/MyCommands.java
@@ -0,0 +1,12 @@
+package com.karmanov;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
+public class MyCommands {
+    @ShellMethod("Add two integers together.")
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
